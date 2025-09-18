e6f5e94a675f5c03f9e25b1d7f15bca8c2b4bc55
Dawn <haidangdavid123@gmail.com>
2025-09-18 17:14:05 +0700

    fix: ts config

--- dist/commands/start.js
+++
@@ -1,3 +0,0 @@
-export default (ctx) => {
-    ctx.reply('Hi! Send me some text and I’ll summarize it. ✨');
-};
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import { bot } from './bot';
-bot.launch();
-console.log('Bot is running');
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
