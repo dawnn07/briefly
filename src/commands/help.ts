1902c0ea09ba4d8a648d0a7a6bafdf529d081a11
Dawn <haidangdavid123@gmail.com>
2025-09-19 00:06:17 +0700

    fix : taixiu

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
 import { Context } from 'telegraf'
 
 export default (ctx: Context) => {
-  ctx.reply('Available commands:\n/start - Start the bot\n/help - Show this help message\n/roast - Roast a random user in the chat\n/summarize - Summarize the chat\nJust send any text and I will summarize it for you! ✨')
+  ctx.reply('Available commands:\n/start - Start the bot\n/help - Show this help message\n/roast - Roast a random user in the chat\n/summarize - Summarize the chat\n/taixiu - Play Tài Xỉu game\nJust send any text and I will summarize it for you! ✨')
 }
